bdbf1edae76dda0908f3207c8dce19780402bb79
Mac Dowell Prosper <macdowell@noukod.com>
2018-08-29 17:29:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,8 @@
 # out the current state.
 def display_board(state_array)
   puts " #{state_array[0]} | #{state_array[1]} | #{state_array[2]} "
-  puts "----------- "
+  puts "-----------"
   puts " #{state_array[3]} | #{state_array[4]} | #{state_array[5]} "
-  puts "----------- "
+  puts "-----------"
   puts " #{state_array[6]} | #{state_array[7]} | #{state_array[8]} "
 end
